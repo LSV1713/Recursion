4f02a93884142152d8791f2bce80238aa19cc82d
SVLoginova <lsvetvik@mail.ru>
2023-11-18 22:45:19 +0300

    Решена задача 68

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int NumSum = 0;
+int GetSum (int start, int end)
+
+{
+    NumSum += start;
+    
+    if (start == end)
+    {
+        return NumSum;
+    }
+    return GetSum(start+1,end);
+    
+}
+Console.WriteLine("Введите число M");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(GetSum(numM,numN));
